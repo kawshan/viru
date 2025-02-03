a3d796cfcbde8ef6ab6a31206ac7a2463f5bef8a
kawshan <kawshan.virantha9999@gmail.com>
2025-02-03 14:52:51 +0530

    version 0 of spring boot application for Viru books pvt ltd --aka initial commit

---
+++ src/main/java/com/virubook/VirubookApplication.java
@@ -0,0 +1,14 @@
+package com.virubook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirubookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirubookApplication.class, args);
+		System.out.println("viru book application started 🔥");
+	}
+
+}
