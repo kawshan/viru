6081892c2f6210802d21ae3bdcbdafd9b9275b75
kawshan <kawshan.virantha9999@gmail.com>
2025-06-12 09:18:54 +0530

    added print in production

--- src/main/resources/static/controllerjs/production.js
+++ src/main/resources/static/controllerjs/production.js
@@ -404,11 +404,121 @@ const deleteProductionDetails = (ob)=>{
 
 
 
+const printProductionHeader = async (ob)=>{
+
+    await refreshProductionDetailsTableForPrint(ob.production_header_key);
+
+
+    const newWindow = window.open();
+    await newWindow.document.write(`
+    
+    <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Production Details</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+<div class="container-fluid" style="position: relative">
+
+    <div class="row">
+        <div class="col-12 text-center"><h4>Production Details</h4></div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-4">
+
+        </div>
+        
+        <div class="col-4"></div>
+        <div class="col-4">
+            <table class="table table-bordered" style="border: 1px solid black; height: 50%">
+                <tbody>
+                <tr>
+                    <td style="font-size: 11px; width: 50%">Production No</td>
+                    <td class="text-end" style="font-size: 12px; width: 50%">${ob.production_header_number}</td>
+                </tr>
+                
+                <tr>
+                    <td style="font-size: 11px; width: 50%">Production Code</td>
+                    <td class="text-end" style="font-size: 12px; width: 50%">${ob.production_header_key}</td>
+                </tr>
+
+                <tr>
+                    <td style="font-size: 11px; width: 50%">Production Date</td>
+                    <td class="text-end" style="font-size: 12px; width: 50%">${new Date(ob.production_header_date).toLocaleString('en-GB', { day: "2-digit", month: "short", year: "2-digit" })}</td>
+                </tr>
+
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+    
+    <div class="row" style="margin-left: 3px; margin-right: 1px">
+    ${tableProductionDetailPrint.outerHTML}
+    </div>
 
 
 
+</div>
+
+<div style="position: absolute; bottom: 1%; width: 100%" >
+    <!--  prepared by, checked by, recieved by area start   -->
+    <div class="row">
+        <div class="col-4 text-start">
+            _____________
+            <p style="font-size: 11px">Prepared By</p>
+        </div>
+        <div class="col-4 text-center">
+            _____________
+            <p style="font-size: 11px">Received By</p>
+        </div>
+        <div class="col-4 text-end">
+            _____________
+            <p style="font-size: 11px; margin-right: 3px">Checked By</p>
+        </div>
+    </div>
+    <!--  prepared by, checked by, recieved by area end   -->
+</div>
 
 
+
+</body>
+</html>
+    `);
+    newWindow.stop();
+    newWindow.print();
+    newWindow.close();
+
+
+    divModifyButton2.classList.add('d-none');
+}
+
+
+const refreshProductionDetailsTableForPrint = (headerKey)=>{
+
+    const productionDetailsList = ajaxGetRequest(`/production-details/findByHeaderKey/${headerKey}`);
+
+    const displayProperty = [
+        {dataType:'function',propertyName:getItemName},
+        {dataType:'text',propertyName:'production_details_description'},
+        {dataType:'function',propertyName:getItemQuantity},
+    ];
+
+
+
+    fillDataIntoTable2(tableProductionDetailPrint,productionDetailsList,displayProperty,false);
+
+}
 
 
 
